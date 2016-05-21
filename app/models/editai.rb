04e3bd2a073a9f0f6a562ff1ca2914e406aff03e
Pedro Santos <pedrosino@gmail.com>
2016-05-21 19:55:33 -0300

    Numero do edital único, expande tipo.

--- app/models/editai.rb
+++ app/models/editai.rb
@@ -2,6 +2,8 @@ class Editai < ActiveRecord::Base
 
   has_many :areas
 
+  validates :numero, uniqueness: true
+
   validate :numero_deve_ter_ano
   def numero_deve_ter_ano
     unless numero.include? "/201"
