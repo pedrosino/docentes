31ffb0aacad102ff0a46b0f2e8957e81fa60c8b9
Pedro Santos <pedrosino@gmail.com>
2016-05-28 21:47:02 -0300

    Factory do edital.

---
+++ spec/factories/editais.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :edital do
+    numero { Faker::Number.number(3).to_s << '/2016' }
+    data { Faker::Date.forward(10) }
+    comeca_inscricao { 10.days.from_now }
+    termina_inscricao { 24.days.from_now }
+  end
+
+end
