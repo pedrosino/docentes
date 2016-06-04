e495981f16e8df1541c921d5a60dc36e923d07f7
Pedro Santos <pedrosino@gmail.com>
2016-06-04 15:15:19 -0300

    Seeds das unidades.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# ruby encoding: utf-8
+
+unidades = [
+['ESEBA','Escola de Educação Básica',''],
+['ESTES','Escola Técnica de Saúde',''],
+['FACED','Faculdade de Educação'],
+['FACIC','Faculdade de Ciências Contábeis'],
+['FACIP','Faculdade de Ciências Integradas do Pontal'],
+['FACOM','Faculdade de Computação'],
+['FADIR','Faculdade de Direito'],
+['FAEFI','Faculdade de Educação Física e Fisioterapia'],
+['FAGEN','Faculdade de Gestão e Negócios'],
+['FAMAT','Faculdade de Matemática'],
+['FAMED','Faculdade de Medicina'],
+['FAMEV','Faculdade de Medicina Veterinária'],
+['FAUED','Faculdade de Arquitetura e Urbanismo e Design'],
+['FECIV','Faculdade de Engenharia Civil'],
+['FEELT','Faculdade de Engenharia Elétrica'],
+['FEMEC','Faculdade de Engenharia Mecânica'],
+['FEQUI','Faculdade de Engenharia Química'],
+['FOUFU','Faculdade de Odontologia'],
+['IARTE','Instituto de Artes'],
+['ICBIM','Instituto de Ciências Biomédicas'],
+['ICIAG','Instituto de Ciências Agrárias'],
+['IEUFU','Instituto de Economia'],
+['IFILO','Instituto de Filosofia'],
+['IGUFU','Instituto de Geografia'],
+['ILEEL','Instituto de Letras e Linguística'],
+['INBIO','Instituto de Biologia'],
+['INCIS','Instituto de Ciências Sociais'],
+['INFIS','Instituto de Física'],
+['INGEB','Instituto de Genética e Bioquímica'],
+['INHIS','Instituto de História'],
+['IPUFU','Instituto de Psicologia'],
+['IQUFU','Instituto de Química']
+]
+
+unidades.each do |sigla,nome|
+  Unidade.create(sigla: sigla, nome: nome)
+end
