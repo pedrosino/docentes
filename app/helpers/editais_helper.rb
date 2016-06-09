82830250b22bde7e1b913cbd9d96f530bc591389
Pedro Santos <pedrosino@gmail.com>
2016-06-08 21:19:09 -0300

    Helper com cidade a partir do campus.

---
+++ app/helpers/editais_helper.rb
@@ -0,0 +1,13 @@
+module EditaisHelper
+  def cidade(campus)
+    {
+      "Educação Física" => "Uberlândia",
+      "Glória" => "Uberlândia",
+      "Monte Carmelo" => "Monte Carmelo",
+      "Patos de Minas" => "Patos de Minas",
+      "Pontal" => "Ituiutaba",
+      "Santa Mônica" => "Uberlândia",
+      "Umuarama" => "Uberlândia"
+    }[campus]
+  end
+end
