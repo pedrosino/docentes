68a156bd26564eb0e5583146e97d6df1c6cd1d56
Pedro Santos <pedrosino@gmail.com>
2016-06-09 17:29:08 -0300

    Factory de titulos.

---
+++ spec/factories/titulos.rb
@@ -0,0 +1,16 @@
+FactoryGirl.define do
+  factory :titulo do
+    descricao { Faker::Lorem.paragraph }
+    valor { Faker::Number.between(1,10) }
+    maximo { Faker::Number.between(10,50) }
+    unidade_medida { Faker::Name.name }
+
+    trait :atividades do
+      tipo 'atividades'
+    end
+
+    trait :producao do
+      tipo 'producao'
+    end
+  end
+end
