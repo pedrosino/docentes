a9e1d4fb14181bb74a2dca6fc927fc1e5aa500f6
Pedro Santos <pedrosino@gmail.com>
2016-06-18 12:02:46 -0300

    Estilos no pdf, factory edital.

--- spec/factories/editais.rb
+++ spec/factories/editais.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :edital do
     numero { Faker::Number.number(3).to_s << '/2016' }
-    data { Faker::Date.forward(10) }
+    data { Faker::Date.forward(10).to_s }
     comeca_inscricao { 10.days.from_now }
     termina_inscricao { 24.days.from_now }
     situacao { Edital::SITUACOES.keys.sample }
